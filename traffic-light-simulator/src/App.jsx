f1397a3fa551f543d5f4814cd831ff349719fe39
Jaydevsinh Chauhan <jaydevchauhan2000@gmail.com>
2025-03-23 21:31:01 +0530

    traffic-light-sims: Created taffic-light-sims

--- traffic-light-simulator/src/App.jsx
+++ traffic-light-simulator/src/App.jsx
@@ -1,5 +1,49 @@
+import { useEffect, useState } from "react";
+
 function App() {
-    return <div>Hello World</div>;
+    const [light, setLight] = useState(["red", "", ""]);
+    const [currLight, setCurrLight] = useState(0);
+
+    useEffect(() => {
+        switch (currLight) {
+            case 0:
+                setLight(["red", "", ""]);
+                break;
+            case 1:
+                setLight(["", "yellow", ""]);
+                break;
+            case 2:
+                setLight(["", "", "green"]);
+                break;
+            default:
+                setLight(["red", "", ""]);
+        }
+    }, [currLight]);
+
+    useEffect(() => {
+        let interval = setInterval(() => {
+            setCurrLight((prevLight) => (prevLight + 1) % 3);
+        }, 1500);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    return (
+        <div className="container">
+            <div
+                className="traffic-light"
+                style={{ backgroundColor: light[0] }}
+            ></div>
+            <div
+                className="traffic-light"
+                style={{ backgroundColor: light[1] }}
+            ></div>
+            <div
+                className="traffic-light"
+                style={{ backgroundColor: light[2] }}
+            ></div>
+        </div>
+    );
 }
 
 export default App;
