aa6ffa92c912c0bae32de440d1e93b6a4670c4d1
Jaydevsinh Chauhan <jaydevchauhan2000@gmail.com>
2025-03-23 19:14:08 +0530

    todo-list: Created todo-list

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -1,5 +1,44 @@
+import { useState } from "react";
+
 function App() {
-    return <div>Hello World</div>;
+    const [task, setTask] = useState("");
+    const [tasks, setTasks] = useState([]);
+
+    const addTask = () => {
+        if (task) {
+            setTasks([...tasks, task]);
+            setTask("");
+        }
+    };
+
+    const deleteTask = (index) => {
+        setTasks(tasks.filter((_, i) => i !== index));
+    };
+
+    return (
+        <div>
+            <h2>To Do List</h2>
+            <input
+                type="text"
+                value={task}
+                onChange={(e) => setTask(e.target.value)}
+                placeholder="Enter a task"
+            />
+            <button type="button" onClick={addTask}>
+                Add
+            </button>
+            <div>
+                {tasks.map((task, index) => {
+                    return (
+                        <div key={index}>
+                            <span>{task}</span>
+                            <button onClick={() => deleteTask(index)}>X</button>
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
 }
 
 export default App;
